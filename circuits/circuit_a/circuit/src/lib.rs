a298115ef01f3709eab3d589f05b22739c22e674
bekauz <bekauz@protonmail.com>
2025-07-24 18:43:57 +0200

    init circuit_a

--- circuits/circuit_a/circuit/src/lib.rs
+++ circuits/circuit_a/circuit/src/lib.rs
@@ -1,15 +1,26 @@
-#![no_std]
-
+// #![no_std]
+#![cfg_attr(not(test), no_std)]
 extern crate alloc;
 
 use alloc::vec::Vec;
+use alloy_rpc_types_eth::EIP1186AccountProofResponse;
 use valence_coprocessor::Witness;
 
 pub fn circuit(witnesses: Vec<Witness>) -> Vec<u8> {
-    let value = witnesses[0].as_data().unwrap();
-    let value = <[u8; 8]>::try_from(value).unwrap();
-    let value = u64::from_le_bytes(value);
-    let value = value.wrapping_add(1);
+    // get the state proof from first witness
+    let state = witnesses[0].as_state_proof().unwrap();
+    // this should contain the destination neutron address
+    let destination = witnesses[1].as_data().unwrap();
 
-    value.to_le_bytes().to_vec()
+    // TODO: think about whether it's possible to easily prevent
+    // users from calling this multiple times. need some one-time
+    // signature or something. but no additional contracts or whatnot.
+    let root = state.root;
+
+    let proof: EIP1186AccountProofResponse = serde_json::from_slice(&state.proof).unwrap();
+
+    let evm_addr = proof.address;
+    let evm_balance = proof.balance;
+
+    alloc::vec::Vec::new()
 }
--- circuits/circuit_a/controller/src/lib.rs
+++ circuits/circuit_a/controller/src/lib.rs
@@ -2,10 +2,14 @@
 
 extern crate alloc;
 
-use alloc::{string::ToString as _, vec::Vec};
+use alloc::{format, string::ToString as _, vec, vec::Vec};
+use alloy_rpc_types_eth::EIP1186AccountProofResponse;
 use serde_json::Value;
-use valence_coprocessor::Witness;
+use valence_coprocessor::{StateProof, Witness};
 use valence_coprocessor_wasm::abi;
+
+const NETWORK: &str = "eth-mainnet";
+const DOMAIN: &str = "ethereum-electra-alpha";
 
 pub fn get_witnesses(args: Value) -> anyhow::Result<Vec<Witness>> {
     abi::log!(
@@ -13,10 +17,30 @@ pub fn get_witnesses(args: Value) -> anyhow::Result<Vec<Witness>> {
         serde_json::to_string(&args).unwrap_or_default()
     )?;
 
-    let value = args["value"].as_u64().unwrap();
-    let value = value.to_le_bytes().to_vec();
+    let block =
+        abi::get_latest_block(DOMAIN)?.ok_or_else(|| anyhow::anyhow!("no valid domain block"))?;
 
-    Ok([Witness::Data(value)].to_vec())
+    let root = block.root;
+    let block = format!("{:#x}", block.number);
+
+    // unpack the destination addr arg
+    let neutron_addr = args["neutron_addr"].as_str().unwrap();
+
+    // TODO: find out how to fetch this
+    let proof: EIP1186AccountProofResponse = EIP1186AccountProofResponse::default();
+    let proof = serde_json::to_vec(&proof)?;
+
+    let ntrn_addr_vec = bincode::serde::encode_to_vec(neutron_addr, bincode::config::standard())?;
+
+    let proof = Witness::StateProof(StateProof {
+        domain: DOMAIN.into(),
+        root,
+        payload: Default::default(),
+        proof,
+    });
+    let neutron_addr_witness = Witness::Data(ntrn_addr_vec);
+
+    Ok(vec![proof, neutron_addr_witness])
 }
 
 pub fn entrypoint(args: Value) -> anyhow::Result<Value> {
